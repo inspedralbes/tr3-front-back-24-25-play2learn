2165a4776130fe151c24f2ebc6f3967cd243cab2
benjamin <a23benromdor@inspedralbes.cat>
2025-03-10 12:08:41 +0100

    Commit inicial correccion

--- play2learn/src/app/games/page.tsx
+++ play2learn/src/app/games/page.tsx
@@ -2,5 +2,5 @@ import ClientComponentGames from "@/app/games/ClientComponentGames";
 
 export default async function Page() {
     await new Promise((resolve) => setTimeout(resolve, 10000));
-    return <ClientComponentGames selectedLanguage={null}/>;
+    return <ClientComponentGames selectedLanguage={''}/>;
 }
