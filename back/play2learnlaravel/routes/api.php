bb7ec00e57710dcecd7ce1cf378aef9ddc809b50
benjamin <a23benromdor@inspedralbes.cat>
2025-03-15 22:52:28 +0100

    Test deploy laravel retry#4

--- back/play2learnlaravel/routes/api.php
+++ back/play2learnlaravel/routes/api.php
@@ -13,6 +13,6 @@ Route::prefix('/auth')->group(function () {
     Route::post('/logout', [\App\Http\Controllers\AuthenticatorController::class, 'logout']);
 });
 
-Route::get('/test', function () {
+Route::get('/te', function () {
     return response()->json(['message' => 'Test route is working!']);
 });
